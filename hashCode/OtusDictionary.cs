ec108afaee8324176d64a6b7395d028e65be4e75
Kurban <kurbanovkk96@mail.ru>
2023-10-06 17:06:53 +0300

    Добавьте файлы проекта.

---
+++ hashCode/OtusDictionary.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace hashCode
+{
+    internal class OtusDictionary
+    {
+        private const int InitialCapacity = 32;
+
+        private Entry[] entries;
+        private int count;
+
+        public OtusDictionary()
+        {
+            entries = new Entry[InitialCapacity];
+        }
+
+        public string this[int key]
+        {
+            get => Get(key);
+            set => Add(key, value);
+        }
+
+        public void Add(int key, string value)
+        {
+            if (count >= entries.Length)
+            {
+                ResizeEntries();
+            }
+
+            if (value == null)
+            {
+                throw new ArgumentNullException(nameof(value), "Значение не может быть Null.");
+            }
+
+            int index = GetIndex(key);
+            while (entries[index] != null)
+            {
+                if (entries[index].Key == key)
+                {
+                    throw new ArgumentException($"Элемент с ключём {key} уже существует.");
+                }
+                index = (index + 1) % entries.Length;
+            }
+
+            entries[index] = new Entry(key, value);
+            count++;
+        }
+
+        public string Get(int key)
+        {
+            int index = GetIndex(key);
+            while (entries[index] != null)
+            {
+                if (entries[index].Key == key)
+                {
+                    return entries[index].Value;
+                }
+                index = (index + 1) % entries.Length;
+            }
+
+            return null;
+        }
+
+        private int GetIndex(int key)
+        {
+            return Math.Abs(key % entries.Length);
+        }
+
+        private void ResizeEntries()
+        {
+            var newEntries = new Entry[entries.Length * 2];
+            foreach (var entry in entries)
+            {
+                if (entry != null)
+                {
+                    int newIndex = GetIndex(entry.Key);
+                    while (newEntries[newIndex] != null)
+                    {
+                        newIndex = (newIndex + 1) % newEntries.Length;
+                    }
+                    newEntries[newIndex] = entry;
+                }
+            }
+            entries = newEntries;
+        }
+
+        private class Entry
+        {
+            public int Key { get; }
+            public string Value { get; }
+
+            public Entry(int key, string value)
+            {
+                Key = key;
+                Value = value;
+            }
+        }
+    }
+}
---
+++ hashCode/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace hashCode
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OtusDictionary dictionary = new OtusDictionary();
+            dictionary[1] = "один";
+            dictionary[2] = "два";
+            dictionary.Add(3, "три");
+            try
+            {
+                dictionary.Add(2, "Два");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+            Console.WriteLine(dictionary[4]);
+            dictionary[4] = "четыре";
+            dictionary[5] = "пять";
+            dictionary[6] = "шесть";
+            dictionary.Add(7, "семь");
+            for (int i = 1; i <= 7; i++)
+            {
+                Console.WriteLine($"Key {i}: {dictionary[i]}");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
